d05616cf90906277f87651e4ed94404026fc6a3c
Saroj <saroj.vrc@gmail.com>
2025-11-25 20:38:01 +0530

    first commit modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 function App() {
-  return <h1 className="text-3xl font-bold text-amber-600">Hello world!</h1>;
+  return <h1 className="text-3xl font-bold text-amber-600">Hello youtube!</h1>;
 }
 
 export default App;
