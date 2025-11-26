bdfc6e8934cc2de3e2ccd1e8c5d8c414d3fccb93
Saroj <saroj.vrc@gmail.com>
2025-11-26 23:51:42 +0530

    ButtonList component

--- src/components/ButtonList.jsx
+++ src/components/ButtonList.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import Button from "./Button";
 
 const ButtonList = () => {
-  return <div>ButtonList</div>;
+  const listofButtons = [
+    "All",
+    "Music",
+    "Gaming",
+    "News",
+    "Sports",
+    "Movies",
+    "Live",
+    "Learning",
+    "Fashion",
+    "Comedy",
+    "Cricket",
+    "Cooking",
+    "Travel",
+    "Technology",
+  ];
+
+  return (
+    <div className="flex">
+      {listofButtons.map((buttonName) => (
+        <Button key={buttonName} name={buttonName} />
+      ))}
+    </div>
+  );
 };
 
 export default ButtonList;
