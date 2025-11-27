30e5e45f695e6d88c7e65b778044fb8906a3872d
Saroj <saroj.vrc@gmail.com>
2025-11-28 00:03:59 +0530

    video container

--- src/components/VideoContainer.jsx
+++ src/components/VideoContainer.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { YouTube_VIDEO_API } from "../utils/constants";
+import VideoCard from "./VideoCard";
+import { Link } from "react-router-dom";
 
 const VideoContainer = () => {
-  return <div>VideoContainer</div>;
+  const [videos, setVideos] = useState([]);
+
+  const getVideos = async () => {
+    const data = await fetch(YouTube_VIDEO_API);
+    const json = await data.json();
+    setVideos(json.items);
+  };
+
+  useEffect(() => {
+    getVideos();
+  }, []);
+
+  return (
+    <div className="flex flex-wrap justify-center rounded-xl">
+      {videos.map((video) => (
+        <Link to={"/watch?v=" + video.id}>
+          <VideoCard key={video.id} info={video} />
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default VideoContainer;
