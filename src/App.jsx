3c09c1242528ab5f5cc63c6d8f22c79302908758
Saroj <saroj.vrc@gmail.com>
2025-11-28 00:01:09 +0530

    path

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,33 @@ import { Provider } from "react-redux";
 import Body from "./components/Body";
 import Header from "./components/Header";
 import store from "./utils/Store";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import WatchPage from "./components/WatchPage";
+import MainContainer from "./components/MainContainer";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Body />,
+    children: [
+      {
+        path: "/",
+        element: <MainContainer />,
+      },
+      {
+        path: "/watch",
+        element: <WatchPage />,
+      },
+    ],
+  },
+]);
 
 const App = () => {
   return (
     <div>
       <Provider store={store}>
         <Header />
-        <Body />
+        <RouterProvider router={appRouter} />
       </Provider>
     </div>
   );
