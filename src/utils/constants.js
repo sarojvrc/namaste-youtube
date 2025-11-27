f3f2e7d1d5352256626d965d49e37431b5ea9203
Saroj <saroj.vrc@gmail.com>
2025-11-28 00:02:29 +0530

    Google API Key added

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,13 @@
-export const MENU_ICON = "https://img.freepik.com/premium-vector/website-navigation-hamburger-menu-icons-set-flat-website-menu-icons-with-rounded-sharp-edges_1254296-1432.jpg";
-export const YOUTUBE_LOGO = "https://logos-world.net/wp-content/uploads/2020/04/YouTube-Logo.png";
-export const USER_ICON = "https://static.vecteezy.com/system/resources/previews/000/574/512/original/vector-sign-of-user-icon.jpg";
+export const MENU_ICON =
+  "https://img.freepik.com/premium-vector/website-navigation-hamburger-menu-icons-set-flat-website-menu-icons-with-rounded-sharp-edges_1254296-1432.jpg";
+export const YOUTUBE_LOGO =
+  "https://logos-world.net/wp-content/uploads/2020/04/YouTube-Logo.png";
+export const USER_ICON =
+  "https://static.vecteezy.com/system/resources/previews/000/574/512/original/vector-sign-of-user-icon.jpg";
+export const Google_API_KEY = "AIzaSyDJgx5kNpmDhatcdgaaf9d-4uuqkNlkcX8";
+export const YouTube_VIDEO_API =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
+  Google_API_KEY;
+
+export const YouTube_SingleVideo_API =
+  "https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&id=";
