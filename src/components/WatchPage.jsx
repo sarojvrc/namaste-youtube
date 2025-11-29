f7b125fea89910db9f9418fa9c7fd05909daab0b
Saroj <saroj.vrc@gmail.com>
2025-11-30 04:47:08 +0530

    added comment container

--- src/components/WatchPage.jsx
+++ src/components/WatchPage.jsx
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import { closeMenu } from "../utils/appSlice";
 import { useSearchParams } from "react-router-dom";
 import { Google_API_KEY, YouTube_SingleVideo_API } from "../utils/constants";
+import CommentsContainer from "./CommentsContainer";
 
 const WatchPage = () => {
   const dispatch = useDispatch();
@@ -27,21 +28,26 @@ const WatchPage = () => {
   }, []);
 
   return (
-    <div className="p-2">
-      <iframe
-        className="ml-10 rounded-xl"
-        width="1000"
-        height="500"
-        src={"https://www.youtube.com/embed/" + searchParam.get("v")}
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerPolicy="strict-origin-when-cross-origin"
-        allowFullScreen
-      ></iframe>
-      <h1 className="font-bold text-2xl p-2 ml-10">
-        {videoData?.snippet?.title}
-      </h1>
+    <div>
+      <div className="p-2">
+        <iframe
+          className="ml-10 rounded-xl"
+          width="1000"
+          height="500"
+          src={"https://www.youtube.com/embed/" + searchParam.get("v")}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+        <h1 className="font-bold text-2xl p-2 ml-10">
+          {videoData?.snippet?.title}
+        </h1>
+      </div>
+      <div className="p-2 ml-10">
+        <CommentsContainer />
+      </div>
     </div>
   );
 };
