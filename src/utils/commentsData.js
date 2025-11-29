0cc6f96130e395f1ee92ba289facc5797920a28c
Saroj <saroj.vrc@gmail.com>
2025-11-30 04:47:19 +0530

    comment dummy data

---
+++ src/utils/commentsData.js
@@ -0,0 +1,165 @@
+export const commentsData = [
+  {
+    name: "Saroj",
+    text: "Great video! Learned a lot.",
+    replies: [
+      {
+        name: "Saroj",
+        text: "Great video! Learned a lot.",
+        replies: [],
+      },
+      {
+        name: "Saroj",
+        text: "Great video! Learned a lot.",
+        replies: [
+          {
+            name: "Saroj",
+            text: "Great video! Learned a lot.",
+            replies: [
+              {
+                name: "Saroj",
+                text: "Great video! Learned a lot.",
+                replies: [
+                  {
+                    name: "Saroj",
+                    text: "Great video! Learned a lot.",
+                    replies: [],
+                  },
+                  {
+                    name: "Saroj",
+                    text: "Great video! Learned a lot.",
+                    replies: [
+                      {
+                        name: "Saroj",
+                        text: "Great video! Learned a lot.",
+                        replies: [],
+                      },
+                      {
+                        name: "Saroj",
+                        text: "Great video! Learned a lot.",
+                        replies: [],
+                      },
+                      {
+                        name: "Saroj",
+                        text: "Great video! Learned a lot.",
+                        replies: [
+                          {
+                            name: "Saroj",
+                            text: "Great video! Learned a lot.",
+                            replies: [],
+                          },
+                          {
+                            name: "Saroj",
+                            text: "Great video! Learned a lot.",
+                            replies: [
+                              {
+                                name: "Saroj",
+                                text: "Great video! Learned a lot.",
+                                replies: [],
+                              },
+                              {
+                                name: "Saroj",
+                                text: "Great video! Learned a lot.",
+                                replies: [
+                                  {
+                                    name: "Saroj",
+                                    text: "Great video! Learned a lot.",
+                                    replies: [],
+                                  },
+                                  {
+                                    name: "Saroj",
+                                    text: "Great video! Learned a lot.",
+                                    replies: [
+                                      {
+                                        name: "Saroj",
+                                        text: "Great video! Learned a lot.",
+                                        replies: [],
+                                      },
+                                      {
+                                        name: "Saroj",
+                                        text: "Great video! Learned a lot.",
+                                        replies: [],
+                                      },
+                                    ],
+                                  },
+                                ],
+                              },
+                            ],
+                          },
+                        ],
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            name: "Saroj",
+            text: "Great video! Learned a lot.",
+            replies: [],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Saroj",
+    text: "Great video! Learned a lot.",
+    replies: [],
+  },
+  {
+    name: "Saroj",
+    text: "Great video! Learned a lot.",
+    replies: [
+      {
+        name: "Saroj",
+        text: "Great video! Learned a lot.",
+        replies: [],
+      },
+      {
+        name: "Saroj",
+        text: "Great video! Learned a lot.",
+        replies: [],
+      },
+      {
+        name: "Saroj",
+        text: "Great video! Learned a lot.",
+        replies: [
+          {
+            name: "Saroj",
+            text: "Great video! Learned a lot.",
+            replies: [],
+          },
+          {
+            name: "Saroj",
+            text: "Great video! Learned a lot.",
+            replies: [
+              {
+                name: "Saroj",
+                text: "Great video! Learned a lot.",
+                replies: [
+                  {
+                    name: "Saroj",
+                    text: "Great video! Learned a lot.",
+                    replies: [],
+                  },
+                  {
+                    name: "Saroj",
+                    text: "Great video! Learned a lot.",
+                    replies: [],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Saroj",
+    text: "Great video! Learned a lot.",
+    replies: [],
+  },
+];
