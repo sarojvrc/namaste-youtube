674780a2a659cff5a475630a8c88e824a5984901
Saroj <saroj.vrc@gmail.com>
2025-11-30 03:42:45 +0530

    search slice added

--- src/utils/store.jsx
+++ src/utils/store.jsx
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import appSlice from "./appSlice";
+import seachSlice from "./searchSlice";
 
 const store = configureStore({
   reducer: {
     app: appSlice,
+    search: seachSlice,
   },
 });
 
